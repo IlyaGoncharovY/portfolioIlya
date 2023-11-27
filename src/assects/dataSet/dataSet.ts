1bd2d043dc1cd53074e0233e24aa889c247c485e
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-27 09:47:59 +0300

    feat(data set) added new file darata - and type

---
+++ src/assects/dataSet/dataSet.ts
@@ -0,0 +1,26 @@
+export type valueHeadersType = "Home" | "Studio" | "Works" | "Contact"
+
+export type headersDataType = {
+    id: number
+    value: valueHeadersType
+}
+export const headersData: headersDataType[] = [
+    {id: 1, value: "Home"},
+    {id: 2, value: "Studio"},
+    {id: 3, value: "Works"},
+    {id: 4, value: "Contact"},
+]
+
+export type valueWorksDataType = "Web Design" | "Development" | "Product Design" | "Social Media"
+
+export type worksDataType = {
+    id: number,
+    value: valueWorksDataType
+}
+
+export const worksData: worksDataType[] = [
+    {id: 1, value: "Web Design"},
+    {id: 2, value: "Development"},
+    {id: 3, value: "Product Design"},
+    {id: 4, value: "Social Media"},
+]
