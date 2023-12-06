e78724d899eb2b29f0a7b6b3198e735b1c7c3878
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-07 02:45:45 +0300

    fix(common app)

--- src/components/works/components/WebDesign.tsx
+++ src/components/works/components/WebDesign.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import LapTopLogo from './3dModel/LapTopLogo';
+import LapTopLogo from './3dModel/Laptop';
 import {CustomCanvas} from './defaultModel/CustomCanvas';
 
 const WebDesign = React.memo(() => {
